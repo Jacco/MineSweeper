1c929529dc6281af20fd48f2c9c9fc6121fd47dc
Jacco Kulman <jaccopk@gmail.com>
2024-07-16 13:23:23 +0200

    update

--- mines.py
+++ mines.py
@@ -1,15 +1,37 @@
+import re
+from dataclasses import dataclass
+
 coords = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+@dataclass
+class Board:
+    height: int
+    width: int
+    mine_count: int
+
+    def __post_init__(self):
+        self.field: list[list[int]] = [[0] * self.width for _ in range(self.height)]
+        self.mines: set[tuple[int,int]] = {(5,5)}
+
+    def __repr__(self):
+        lines: list[str] = []
+        for y in range(self.height):
+            line = ''.join("💣" if (y,x) in  self.mines else "  " for x in range(self.width))
+            lines.append(line)
+        return "\n".join(lines)
+
 class Game:
-    def __init__(self, height: int, width: int, mines: int):
-        self.board = coords[:width] + '\n' + ('*' * width + '\n') * height
+    def __init__(self, height: int, width: int, mine_count: int):
+        self.board = Board(height, width, mine_count)
 
     def get_input(self):
-        return input("row col [mark]: ")
+        row, col, *mark = [coords.find(v.upper()) for v in re.split(r'\s+', input('row col [mark]: '))]
+        return row, col, bool(mark)
 
     def play(self):
         while True:
             print(self.board)
-            self.get_input()
+            row, col, mark = self.get_input()
+            print(row, col, mark)
 
 Game(10, 10, 5).play()
