ddeec4579d0ffab37838b6f0da5a6cf278356bd8
Jacco Kulman <jaccopk@gmail.com>
2024-07-16 09:52:52 +0200

    update

--- mines.py
+++ mines.py
@@ -1,3 +1,5 @@
+import re
+
 coords = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 class Game:
@@ -5,11 +7,13 @@ class Game:
         self.board = coords[:width] + '\n' + ('*' * width + '\n') * height
 
     def get_input(self):
-        return input("row col [mark]: ")
+        row, col, *mark = [coords.find(v.upper()) for v in re.split(r'\s+', input('row col [mark]: '))]
+        return row, col, bool(mark)
 
     def play(self):
         while True:
             print(self.board)
-            self.get_input()
+            row, col, mark = self.get_input()
+            print(row, col, mark)
 
 Game(10, 10, 5).play()
