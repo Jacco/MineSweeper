4d8468ab7c1fc5c7283b745050846d45fdef156e
Jacco Kulman <jaccopk@gmail.com>
2024-07-16 09:04:20 +0200

    improved

--- mines.py
+++ mines.py
@@ -1,6 +1,8 @@
+coords = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 class Game:
     def __init__(self, height: int, width: int, mines: int):
-        self.board = ('*' * width + '\n') * height
+        self.board = coords[:width] + '\n' + ('*' * width + '\n') * height
 
     def get_input(self):
         return input("row col [mark]: ")
